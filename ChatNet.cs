9677e8d8e870574a15f271c1c31c7ff99c02cf33
MaTBeuKo <56413834+MaTBeuKo@users.noreply.github.com>
2020-06-04 15:15:50 +0300

    Add files via upload
    Chat scripts, full project - https://yadi.sk/d/ZU-q2453KSVPFQ

---
+++ ChatNet.cs
@@ -0,0 +1,100 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Photon.Realtime;
+using System.IO;
+
+public class ChatNet : MonoBehaviour
+{
+    GameObject chat;                                        
+    static public PhotonView photonView;
+    GameObject MessageInput;
+    Player[] playerlist;
+
+    private void Awake()
+    {
+
+    }
+    public void Start()
+    {
+        MessageInput = GameObject.Find("ChatInput");
+        photonView = GetComponent<PhotonView>();
+        chat = GameObject.Find("ChatText");
+    }
+
+    public void Send()                                                                                              //отправить сообщение
+    {
+
+        if (MessageInput.GetComponent<Text>().text != "")
+        {
+            if (MessageInput.GetComponent<Text>().text[0] != '.')
+            {
+                photonView.RPC("SendMessage", RpcTarget.AllViaServer, Manager.MyNickName, MessageInput.GetComponent<Text>().text);
+
+            }
+            else
+            {                                                                                                       //или выполнить команду
+                
+                playerlist = PhotonNetwork.PlayerList;
+                if (MessageInput.GetComponent<Text>().text == ".list")                                              //вывести список участников чата
+                {
+                    chat.GetComponent<Text>().text += "Players online:\n";
+                    for (int i = 0; i < playerlist.Length; i++)
+                    {
+                        chat.GetComponent<Text>().text += playerlist[i].NickName;
+                        if (playerlist[i].IsMasterClient)
+                            chat.GetComponent<Text>().text += " (Owner)";
+                        chat.GetComponent<Text>().text += ", ";
+                    }
+                    chat.GetComponent<Text>().text += "\n";
+                }
+
+                else if (MessageInput.GetComponent<Text>().text.Split()[0] == ".kick")                              //выгнать участника
+                {
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        for (int i = 0; i < playerlist.Length; i++)
+                        {
+                            if (playerlist[i].NickName == MessageInput.GetComponent<Text>().text.Split()[1])
+                            {
+                                PhotonNetwork.CloseConnection(playerlist[i]);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        chat.GetComponent<Text>().text += "Only creator able to kick \n";
+                    }
+                }
+                else if (MessageInput.GetComponent<Text>().text == ".clear")                                        //очистить чат
+                {
+                    chat.GetComponent<Text>().text = "";
+                }
+                else if (MessageInput.GetComponent<Text>().text == ".help")                                         //помощь
+                {
+                    chat.GetComponent<Text>().text += "Commands list: \n.list - list of the players in your room \n.kick (playername) - for room creator only \n.clear - clear the chat \n";
+                }
+
+            }
+        }
+        GameObject.Find("ChatInputField").GetComponent<InputField>().text = "";
+    }
+
+
+
+    int i = 1;
+    [PunRPC]
+    void SendMessage(string name, string mes)                                                           //выводит полученное сообщение
+    {
+        chat.GetComponent<Text>().text += $"{DateTime.Now.Hour}:{ DateTime.Now.Minute} {name}: {mes}";
+        chat.GetComponent<Text>().text += "\n";
+    }
+
+
+}
---
+++ Manager.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using UnityEngine.Networking;
+
+public class Manager : MonoBehaviourPunCallbacks
+{
+    GameObject NickNameField;                                       
+    GameObject ChatText;
+    GameObject RoomNameField;
+    GameObject RoomName;
+    GameObject Create;
+    GameObject AutoConnectToggle;
+    GameObject[] pausebuts;
+    bool isPaused = false;
+
+
+    static public string MyNickName;
+
+    public void Awake()                                                 //скрываем пауз меню
+    {
+        AutoConnectToggle = GameObject.Find("AutoConnectToggle");
+        pausebuts = GameObject.FindGameObjectsWithTag("hideobjects");
+        foreach (GameObject gameobj in pausebuts)
+        {
+            gameobj.SetActive(false);
+        }
+    }
+
+    private void Start()                                                //инизиализация
+    {
+        ChatText = GameObject.Find("ChatText");
+        RoomNameField = GameObject.Find("RoomNameField123");
+        RoomName = GameObject.Find("RoomName");
+        Create = GameObject.Find("Create");
+        NickNameField = GameObject.Find("NickNameField");
+        GameObject.Find("NickName").GetComponent<InputField>().text = PlayerPrefs.GetString("NickName");    //выставляем пользовательские настройки
+
+        if (PlayerPrefs.GetInt("AutoConnect") == 1)
+        {
+            AutoConnectToggle.GetComponent<Toggle>().isOn = true;
+            ConnectToMaster();
+        }
+        RoomName.GetComponent<InputField>().text = PlayerPrefs.GetString("RoomName");
+
+    }
+    public void ConnectToMaster()                                                                               //подключение к серверам фотона
+    {
+        if (PlayerPrefs.HasKey("NickName"))
+            NickNameField.GetComponent<Text>().text = PlayerPrefs.GetString("NickName");
+
+
+        if (NickNameField.GetComponent<Text>().text != "")
+        {
+            PhotonNetwork.NickName = NickNameField.GetComponent<Text>().text;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            log("Enter your name");
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+        GameObject.Find("NickName").GetComponent<InputField>().readOnly = true;
+        NickNameField.GetComponent<Text>().color = Color.green;
+        
+        MyNickName = PhotonNetwork.NickName;
+        log("Logged in!");
+        PlayerPrefs.SetString("NickName", MyNickName);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        if (message == "No match found")
+            log("No available rooms found");
+        else
+            log(message + returnCode.ToString());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        ChatText.GetComponent<Text>().text = "";
+        if (PhotonNetwork.IsMasterClient)
+        {
+            GameObject.Find("RoomName").GetComponent<InputField>().text = PhotonNetwork.CurrentRoom.Name;
+            log($"Room {PhotonNetwork.CurrentRoom.Name} created, players can join");
+            GameObject.Find("Create").GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            log("Connected to " + PhotonNetwork.CurrentRoom.Name);
+            PlayerPrefs.SetString("RoomName", PhotonNetwork.CurrentRoom.Name);
+        }
+        RoomName.GetComponent<InputField>().readOnly = true;
+        RoomNameField.GetComponent<Text>().color = Color.yellow;
+        PlayerPrefs.SetString("RoomName", PhotonNetwork.CurrentRoom.Name);
+
+    }
+    public override void OnLeftRoom()
+    {
+        RoomName.GetComponent<InputField>().readOnly = false;
+        RoomNameField.GetComponent<Text>().color = Color.black;
+        ChatText.GetComponent<Text>().text = "";
+        Create.GetComponent<Button>().interactable = true;
+        RoomName.GetComponent<InputField>().text = "";
+    }
+
+    public void CreateRoom()
+    {
+        if (RoomNameField.GetComponent<Text>().text == "")
+            PhotonNetwork.CreateRoom(PhotonNetwork.NickName, new RoomOptions { MaxPlayers = 5 });
+        else
+            PhotonNetwork.CreateRoom(RoomNameField.GetComponent<Text>().text, new RoomOptions { MaxPlayers = 5 });
+    }
+
+
+    public void JoinRoom()
+    {
+        if (RoomNameField.GetComponent<Text>().text != "")
+            PhotonNetwork.JoinRoom(RoomNameField.GetComponent<Text>().text);
+        else
+            PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        if (message == "Game does not exist")
+            log("Room does not exist!");
+        else
+            log($"{ message} {returnCode.ToString()}");
+
+    }
+    public void LeaveRoom()
+    {
+        PlayerPrefs.SetString("RoomName", PhotonNetwork.CurrentRoom.Name);
+        PhotonNetwork.LeaveRoom();
+        
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        log(newPlayer.NickName + " Joined");
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        log(otherPlayer.NickName + " Left");
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        log(message);
+    }
+
+    public void log(string mes)                                                     
+    {
+        ChatText.GetComponent<Text>().text += mes;
+        ChatText.GetComponent<Text>().text += "\n";
+    }
+
+    public void Pause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            foreach (GameObject gameobj in pausebuts)
+            {
+                gameobj.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (GameObject gameobj in pausebuts)
+            {
+                gameobj.SetActive(false);
+            }
+        }
+    }
+    public void AppQuit()
+    {
+        SavePrefs();
+        if (PhotonNetwork.InRoom)
+        LeaveRoom();
+        Application.Quit(0);
+    }
+
+    public void SavePrefs()
+    {
+        PlayerPrefs.Save();
+    }
+
+    public void AutoConnect()                                       
+    {
+        if (AutoConnectToggle.GetComponent<Toggle>().isOn)
+            PlayerPrefs.SetInt("AutoConnect", 1);
+        else
+            PlayerPrefs.SetInt("AutoConnect", 0);
+    }
+}
